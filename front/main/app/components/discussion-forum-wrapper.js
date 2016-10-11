9f1d865954c370f722b29b809520b62cc7e2508f
Karol Tatala <ktatala@wikia-inc.com>
2016-10-11 13:35:21 +0200

    SOC-3167 | POC grouping posts for better UX

--- front/main/app/components/discussion-forum-wrapper.js
+++ front/main/app/components/discussion-forum-wrapper.js
@@ -4,5 +4,19 @@ import ResponsiveMixin from '../mixins/responsive';
 
 export default Ember.Component.extend(
 	DiscussionWrapperComponentMixin,
-	ResponsiveMixin
+	ResponsiveMixin,
+	{
+		pages: Ember.computed('posts.[]', function () {
+			var pages = [];
+			for(let i = 0; i < this.get('posts').length; i+=20) {
+				pages.push(this.get('posts').slice(i, i+20));
+			}
+			return pages;
+		}),
+
+		init() {
+			this._super(...arguments);
+			// setTimeout(() => { this.get('pages'); }, 3000);
+		}
+	}
 );
