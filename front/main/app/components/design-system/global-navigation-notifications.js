3df791ab72717b8d5ce6d0dc2457f02fb565177e
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-01-27 16:49:24 +0100

    SOC-3839 | Starting work on formatting messages

--- front/main/app/components/design-system/global-navigation-notifications.js
+++ front/main/app/components/design-system/global-navigation-notifications.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	hasUnread: true,
 	tagName: '',
 	notifications: Ember.inject.service(),
 
 	notificationsList: Ember.computed('notifications.data', function () {
-		debugger;
 		return this.get('notifications.data');
 	})
 });
