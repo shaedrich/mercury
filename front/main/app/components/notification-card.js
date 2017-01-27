dc393bffd24625c9994af02b8fbff559a72c4ba7
bkoval <bartlomiej.kowalczyk@wikia-inc.com>
2017-01-27 16:49:24 +0100

    SOC-3839 | Starting work on formatting messages

--- front/main/app/components/notification-card.js
+++ front/main/app/components/notification-card.js
@@ -3,5 +3,32 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	tagName: 'li',
 
-	text: 'Lorem ipsum gówno'
+	text: 'I am a totally harmless text!',
+
+	formatters: [],
+
+	getReplyMessageBody(model) {
+		const hasTitle = model.title,
+			hasTwoUsers = model.events.length === 2,
+			hasThreeOrMoreUsers = model.events.length === 2;
+
+		if (hasTitle) {
+			if (hasThreeOrMoreUsers) {
+				return;
+			} else if (hasTwoUsers) {
+
+			} else {
+
+			}
+		} else {
+			if (hasThreeOrMoreUsers) {
+				return;
+			} else if (hasTwoUsers) {
+
+			} else {
+
+			}
+		}
+
+	}
 });
