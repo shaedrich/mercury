df78926948fa24b8088c6507a62744d92fa08a23
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-02-15 09:19:42 +0100

    use devDomain value from settings in lib

--- server/app/lib/utils.js
+++ server/app/lib/utils.js
@@ -113,7 +113,7 @@ export function getWikiDomainName(settings, hostName = '') {
 		const regex = /^\.?(.+?)\.((?:[\d]{1,3}\.){3}[\d]{1,3}\.xip.io)$/,
 			match = hostName.match(regex);
 
-		return match ? `${match[1]}.${settings.devboxDomain}.wikia-dev.[us|pl]` : hostName;
+		return match ? `${match[1]}.${settings.devboxDomain}.wikia-dev.${settings.devDomain}` : hostName;
 	} else {
 		return hostName;
 	}
