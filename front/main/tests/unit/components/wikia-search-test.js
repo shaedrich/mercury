e3c972cc16c33ed69c513618e464b9748a8ea4f1
Igor Rogatty <igor@wikia-inc.com>
2017-02-27 12:58:51 +0100

    XW-2870 | normalize search suggestions URIs

--- front/main/tests/unit/components/wikia-search-test.js
+++ front/main/tests/unit/components/wikia-search-test.js
@@ -134,17 +134,17 @@ test('setSearchSuggestionItems - correctly set suggestions array', function (ass
 			{
 				text: '<span class="wikia-search__suggestion-highlighted">sug</span>gestion 2!',
 				title: 'suggestion 2!',
-				uri: '/wiki/suggestion%202!'
+				uri: '/wiki/suggestion_2!'
 			},
 			{
 				text: '<span class="wikia-search__suggestion-highlighted">sug</span> GES %^&*^%&>?<tion>',
 				title: 'sug GES %^&*^%&>?<tion>',
-				uri: '/wiki/sug%20GES%20%25%5E%26*%5E%25%26%3E%3F%3Ction%3E'
+				uri: '/wiki/sug_GES_%25%5E%26*%5E%25%26%3E%3F%3Ction%3E'
 			},
 			{
 				text: 'no query inside',
 				title: 'no query inside',
-				uri: '/wiki/no%20query%20inside'
+				uri: '/wiki/no_query_inside'
 			},
 			{
 				text: '<span class="wikia-search__suggestion-highlighted">sug</span> ' +
@@ -153,7 +153,7 @@ test('setSearchSuggestionItems - correctly set suggestions array', function (ass
 				'<span class="wikia-search__suggestion-highlighted">sug</span> ' +
 				'<span class="wikia-search__suggestion-highlighted">sug</span>',
 				title: 'sug sug suggestions sug sug',
-				uri: '/wiki/sug%20sug%20suggestions%20sug%20sug'
+				uri: '/wiki/sug_sug_suggestions_sug_sug'
 			}
 		];
 
