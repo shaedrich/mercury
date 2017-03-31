54c18048bbbd1f4aa9cc7b326b21c52a3b5b16cd
Michał Turek <mturek@wikia-inc.com>
2017-03-31 18:29:34 +0200

    IRIS-4231 | Ugly sign out button which redirects to /d

---
+++ front/main/app/components/sign-out-button.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import {track, trackActions} from 'common/utils/track';
+
+const {Component, computed, inject} = Ember;
+
+export default Component.extend(
+	{
+		classNames: ['wds-sign-out-button'],
+
+
+		currentUrl: null,
+		onPathChanged: Ember.observer('target.url', function() {
+			Ember.run.next(this, function() {
+				this.set('currentUrl', window.location.href);
+			});
+		}),
+
+		actions: {
+			getBack() {
+				this.sendAction('setDrawerContent', 'nav');
+			},
+		}
+	}
+);
