7a79ff29ea7f64d5dce34cb3a98bf43681041a1b
Michał Turek <mturek@wikia-inc.com>
2017-04-04 16:51:34 +0200

    IRIS-4231 | If user is unauthenticated redirect anyways

--- server/app/facets/auth/signout.js
+++ server/app/facets/auth/signout.js
@@ -1,5 +1,5 @@
 import signOutUser from '../operations/signout';
-import HttpStatus from 'http-status-codes';
+import Logger from '../../lib/logger';
 import {getRedirectUrlForPost} from '../../lib/auth-url-factory';
 /**
  * @param {Hapi.Request} request
@@ -8,17 +8,21 @@ import {getRedirectUrlForPost} from '../../lib/auth-url-factory';
  */
 export default function post(request, reply) {
 	// TODO IRIS-4012: Implement anti-CSRF token
+	const redirectUrl = getRedirectUrlForPost(request);
 	if (!request.auth.isAuthenticated) {
-		reply('Cannot sign out an unauthenticated user.').code(HttpStatus.BAD_REQUEST);
+		Logger.warn(`User is not authenticated but sign out has been invoked.
+			This may be correct if for example user logged out in another tab.
+			Redirecting to target URL anyways.`);
+		reply.redirect(redirectUrl).takeover();
 	} else {
 		signOutUser(request)
 			.then(result => {
 				// For redirect Hapi uses 302 status code which should correctly change the method to GET
-				reply.redirect(getRedirectUrlForPost(request))
+				reply.redirect(redirectUrl)
 					.state('access_token', result.token)
 					.takeover();
 			}).catch((result) => {
-				reply('Sign out unsuccessful').code(result.status);
-			});
+			reply('Sign out unsuccessful').code(result.status);
+		});
 	}
 }
