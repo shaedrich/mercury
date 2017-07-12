3d70079dee6c010faa7ea83d723b3825a33a03d8
Karol Tatała <ktatala@wikia-inc.com>
2017-07-12 14:10:06 +0200

    XW-3630 | local nav use model data

--- front/main/app/components/design-system/local-navigation-dropdown-level-2.js
+++ front/main/app/components/design-system/local-navigation-dropdown-level-2.js
@@ -5,6 +5,6 @@ const {Component, computed} = Ember;
 export default Component.extend({
 	tagName: '',
 	stickedClassName: computed('model', 'currentIndex', function () {
-		return (this.get('model').submenu.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
+		return (this.get('model').items.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
 	})
 });
--- front/main/app/components/design-system/wds-dropdown.js
+++ front/main/app/components/design-system/wds-dropdown.js
@@ -3,13 +3,24 @@ import Ember from 'ember';
 const {Component} = Ember;
 
 export default Component.extend({
-	classNames: ['wds-dropdown'],
-
-	onMouseEnter() {},
+	classNameBindings: ['isClicked:wds-is-clicked', 'dropdownExpanded:wds-is-active', 'hasShadow:wds-has-shadow', 'hasDarkShadow:wds-has-dark-shadow', 'isLevel2:wds-dropdown-level-2:wds-dropdown'],
+	isLevel2: false,
+	isTouchDevice: Ember.computed(function () {
+		return ('ontouchstart' in window);
+	}),
 
 	actions: {
-		mouseEnter() {
-			this.get('onMouseEnter')();
+		click(e) {
+			if (this.get('isTouchDevice') && !this.get('isClicked')) {
+				this.set('isClicked', true);
+				e.preventDefault();
+			}
+		},
+
+		mouseLeave() {
+			if (this.get('isTouchDevice')) {
+				this.set('isClicked', false);
+			}
 		}
 	}
 });
