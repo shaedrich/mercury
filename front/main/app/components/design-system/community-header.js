6c6b67cae70717ae6b261ea788d8491c61aed52c
Igor Rogatty <igor@wikia-inc.com>
2017-07-12 13:18:00 +0200

    XW-3630 | use community header data from DS API

--- front/main/app/components/design-system/community-header.js
+++ front/main/app/components/design-system/community-header.js
@@ -1,32 +1,9 @@
 import Ember from 'ember';
-import Headroom from '../../mixins/headroom';
 import {track, trackActions} from 'common/utils/track';
 
 const {Component, Object: EmberObject, computed} = Ember;
 
 export default Component.extend({
 	communityHeaderStyle: new Ember.Handlebars.SafeString(`background-image: url(http://static.wikia.nocookie.net/c4a1302b-476b-468d-8766-2f36a6ace398);`),
-	model: {
-		wordmark: {
-			image: {
-				url: 'http://img3.wikia.nocookie.net/__cb23/starwars/images/8/89/Wiki-wordmark.png',
-				width: '250',
-				height: '65',
-			},
-			label: 'Wookiepedia',
-			href: 'http://google.pl',
-		},
-		sitename: {
-			href: 'http://google.pl',
-			label: {
-				value: 'Wookiepedia'
-			}
-		},
-		counter: {
-			value: 1234,
-			label: {
-				key: 'asd'
-			}
-		}
-	}
+	model: EmberObject.create(M.prop('communityHeader'))
 });
