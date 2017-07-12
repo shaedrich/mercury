140441346fdfa382f262e776ca045013c83a2629
Igor Rogatty <igor@wikia-inc.com>
2017-07-12 16:51:25 +0200

    XW-3630 | use background image from API

--- front/main/app/components/design-system/community-header.js
+++ front/main/app/components/design-system/community-header.js
@@ -1,9 +1,23 @@
 import Ember from 'ember';
 import {track, trackActions} from 'common/utils/track';
 
-const {Component, Object: EmberObject, computed} = Ember;
+const {
+	Component,
+	Handlebars: {SafeString},
+	Object: EmberObject,
+	computed
+} = Ember;
 
 export default Component.extend({
-	communityHeaderStyle: new Ember.Handlebars.SafeString(`background-image: url(http://static.wikia.nocookie.net/c4a1302b-476b-468d-8766-2f36a6ace398);`),
+	tagName: 'header',
+	classNames: ['wds-community-header'],
+	attributeBindings: ['style'],
+
+	style: computed('model', function () {
+		const backgroundImage = this.get('model.background_image'),
+			style = `background-image: url('${backgroundImage}');`;
+
+		return new SafeString(style);
+	}),
 	model: EmberObject.create(M.prop('communityHeader'))
 });
