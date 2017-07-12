c767a4079cc6c2a7203a7f8f385d8f12bd91a49c
Igor Rogatty <igor@wikia-inc.com>
2017-07-12 14:32:34 +0200

    XW-3630 | remove excessive components

--- front/main/app/components/design-system/local-navigation-dropdown-level-2.js
+++ front/main/app/components/design-system/local-navigation-dropdown-level-2.js
@@ -5,6 +5,7 @@ const {Component, computed} = Ember;
 export default Component.extend({
 	tagName: '',
 	stickedClassName: computed('model', 'currentIndex', function () {
-		return (this.get('model').items.length < this.get('currentIndex') + 1) ? 'wds-is-sticked-to-parent' : '';
+		return this.get('model.items.length') < this.get('currentIndex') + 1 ?
+			'wds-is-sticked-to-parent' : '';
 	})
 });
--- front/main/app/components/design-system/wds-tab.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-const {Component} = Ember;
-
-export default Component.extend({
-	tagName: 'li',
-	classNames: 'wds-tabs__tab'
-});
