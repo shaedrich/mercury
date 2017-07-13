95e3d623fd5e9f6d25cea7ed04e1aefc9c16b9e7
Igor Rogatty <igor@wikia-inc.com>
2017-07-13 12:15:17 +0200

    XW-3630 | fix label for number of posts

--- front/main/app/components/design-system/community-header.js
+++ front/main/app/components/design-system/community-header.js
@@ -13,11 +13,20 @@ export default Component.extend({
 	classNames: ['wds-community-header'],
 	attributeBindings: ['style'],
 
+	model: EmberObject.create(M.prop('communityHeader')),
 	style: computed('model', function () {
 		const backgroundImage = this.get('model.background_image'),
 			style = `background-image: url('${backgroundImage}');`;
 
 		return new SafeString(style);
 	}),
-	model: EmberObject.create(M.prop('communityHeader'))
+	numberOfPostsLabel: computed('numberOfPosts', function () {
+		const key = this.get('numberOfPosts') === 1 ?
+			'community-header-discussions-post' :
+			'community-header-discussions-posts';
+
+		return i18n.t(key, {
+			ns: 'design-system'
+		});
+	})
 });
