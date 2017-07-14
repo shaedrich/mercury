f4fe3666d5dac6ec4399bf1eea405f9f7f826cb5
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-07-14 15:50:06 +0200

    XW-3753: avatars WIP

--- front/main/app/components/design-system/community-header.js
+++ front/main/app/components/design-system/community-header.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-import {track, trackActions} from 'common/utils/track';
+import Ember from "ember";
+import {track, trackActions} from "common/utils/track";
 
 const {
 	Component,
@@ -27,6 +27,41 @@ export default Component.extend({
 
 		return i18n.t(key, {
 			ns: 'design-system'
+		});
+	}),
+
+	contributors: computed(function () {
+		return [{
+			"id": "29636668",
+			"avatarUrl": "http://static.wikia.nocookie.net/29254630-17a1-48de-b179-2a0c41714154/scale-to-width-down/100",
+			"name": "Vault-Tec Staff",
+			"badgePermission": ""
+		}, {
+			"id": "32377837",
+			"avatarUrl": "https://static.wikia.nocookie.net/7c9209f2-6033-45ff-b2e9-2e4902c32def",
+			"name": "VertigoIndygo",
+			"badgePermission": ""
+		}, {
+			"id": "32438230",
+			"avatarUrl": "https://static.wikia.nocookie.net/7d238f98-b47c-4540-91cc-5438e4ed9662",
+			"name": "Atlas 1738",
+			"badgePermission": ""
+		}, {
+			"id": "31997683",
+			"avatarUrl": null,
+			"name": "Naja1234",
+			"badgePermission": ""
+		}, {
+			"id": "31021421",
+			"avatarUrl": "http://static.wikia.nocookie.net/29622b38-1ecf-4df8-ba14-9a3ca87a81db/scale-to-width-down/100",
+			"name": "Motherfukerjones",
+			"badgePermission": ""
+		}].slice(0, 5).map(function (item) {
+			item['url'] = M.buildUrl({
+										 namespace: 'User',
+										 title: item['name'],
+									 });
+			return item;
 		});
 	})
 });
