675b61562127ecaf94881466489a50ebee1ca419
Karol Tatała <ktatala@wikia-inc.com>
2017-07-14 15:45:00 +0200

    XW-3631 | reuse user-avatar component

--- front/main/app/components/left-rail-user-details.js
+++ front/main/app/components/left-rail-user-details.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	classNames: ['left-rail-user-details'],
+
 	userProfile: Ember.computed('users', function () {
 		let users = (this.get('users'));
 		return users[0];
