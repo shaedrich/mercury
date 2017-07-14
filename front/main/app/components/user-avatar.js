2939d29420606ffbdc0b34b8113f683405aa5ade
Igor Rogatty <igor@wikia-inc.com>
2017-07-14 17:19:52 +0200

    XW-3633 | don't display badges in wds-avatar-stack

--- front/main/app/components/user-avatar.js
+++ front/main/app/components/user-avatar.js
@@ -1,24 +1,30 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {Component, computed} = Ember;
+
+export default Component.extend({
 	classNames: ['user-avatar'],
-	profileName: Ember.computed('username', function () {
+
+	shouldWrapInHref: true,
+
+	profileName: computed('username', function () {
 		const userName = this.get('username') || '';
 
 		return userName.trim();
 	}),
+
 	/**
 	 * Returns link to the post author's user page
 	 * @returns {string}
 	 */
-	profileUrl: Ember.computed('profileName', function () {
+	profileUrl: computed('profileName', function () {
 		return M.buildUrl({
 			namespace: 'User',
 			title: this.get('profileName'),
 		});
 	}),
-	displayName: Ember.computed('profileName', function () {
+
+	displayName: computed('profileName', function () {
 		return this.get('anonymous') ? i18n.t('app.username-anonymous') : this.get('profileName');
-	}),
-	shouldWrapInHref: true
+	})
 });
