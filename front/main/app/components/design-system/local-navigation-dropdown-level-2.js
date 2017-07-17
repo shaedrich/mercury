caf1b748f468a5f6ba57d21ec595f31dc973aa54
JamesHawking <jakubj91@gmail.com>
2017-07-17 14:59:03 +0200

    XW-3631 | Variable name change

--- front/main/app/components/design-system/local-navigation-dropdown-level-2.js
+++ front/main/app/components/design-system/local-navigation-dropdown-level-2.js
@@ -4,8 +4,9 @@ const {Component, computed} = Ember;
 
 export default Component.extend({
 	tagName: '',
-	stickedClassName: computed('model', 'currentIndex', function () {
-		return this.get('model.items.length') < this.get('currentIndex') + 1 ?
+	stickedClassName: computed('item', 'currentIndex', function () {
+		console.log(this.get('item'));
+		return this.get('item.items.length') < this.get('currentIndex') + 1 ?
 			'wds-is-sticked-to-parent' : '';
 	})
 });
