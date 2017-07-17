e7def82e84b545441048fa83713a9bfacff5e13e
JamesHawking <jakubj91@gmail.com>
2017-07-17 15:00:14 +0200

    XW-3631 | Typo

--- front/main/app/components/design-system/local-navigation-dropdown-level-2.js
+++ front/main/app/components/design-system/local-navigation-dropdown-level-2.js
@@ -5,7 +5,6 @@ const {Component, computed} = Ember;
 export default Component.extend({
 	tagName: '',
 	stickedClassName: computed('item', 'currentIndex', function () {
-		console.log(this.get('item'));
 		return this.get('item.items.length') < this.get('currentIndex') + 1 ?
 			'wds-is-sticked-to-parent' : '';
 	})
