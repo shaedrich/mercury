d677c5ebb9ea09624d5220a5112804af9f81d191
Mateusz Rybarski <mateusz.rybarski@wikia-inc.com>
2017-07-18 12:55:42 +0200

    XW-3632: track user-avatar clicks on all posts by user page

--- front/main/app/components/left-rail-user-details.js
+++ front/main/app/components/left-rail-user-details.js
@@ -1,9 +1,24 @@
 import Ember from 'ember';
+import {track, trackActions} from 'common/utils/track';
 
 const {Component, computed: {reads}} = Ember;
 
 export default Component.extend({
 	classNames: ['left-rail-user-details'],
 
-	userProfile: reads('users.0')
+	userProfile: reads('users.0'),
+
+	didInsertElement() {
+		this.$().on('click', '[data-tracking-label]', (e) => {
+			const label = this.$(e.currentTarget).data('tracking-label');
+
+			track({
+				action: trackActions.click,
+				category: 'DesktopWebDiscussions',
+				label: label
+			});
+
+			e.stopPropagation();
+		});
+	}
 });
