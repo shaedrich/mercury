f110bf5649f91229886184a9bd1c632c7059257e
Igor Rogatty <igor@wikia-inc.com>
2017-07-20 14:35:01 +0200

    XW-3739 | get vars from query params and pass to sass compiler

--- server/app/facets/operations/sass.js
+++ server/app/facets/operations/sass.js
@@ -4,12 +4,28 @@ import sass from 'node-sass';
 import Logger from '../../lib/logger';
 
 export default (request, reply) => {
+	const whitelist = [
+			'color-community-header',
+			'color-links',
+			'color-page'
+		],
+		sassVariables = Object.keys(request.query)
+			.filter((key) => {
+				return whitelist.indexOf(key) > -1 &&
+					request.query[key].length > 0;
+			})
+			.map((key) => {
+				// FIXME escape the value
+				return `$${key}: ${request.query[key]};`;
+			})
+			.join('');
+
 	sass.render({
 		includePaths: [
 			'front/main/app/styles/',
 			'front/'
 		],
-		data: `@import 'app.scss';`,
+		data: `${sassVariables} @import 'app.scss';`,
 		outputStyle: 'compressed',
 		sourceComments: 'none'
 	}, (err, result) => {
