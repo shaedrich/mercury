3ba9fc232916dac70fe3ca284293d3a3e99170e7
JamesHawking <jakubj91@gmail.com>
2017-07-21 13:06:20 +0200

    XW-3627 | Remove unused discussion-community-badge component

--- front/main/app/controllers/discussion/moderator/user-activity.js
+++ front/main/app/controllers/discussion/moderator/user-activity.js
@@ -2,6 +2,7 @@ import DiscussionModerationControllerMixin from '../../../mixins/discussion-mode
 import DiscussionContributionControllerMixin from '../../../mixins/discussion-contribution-controller';
 import ResponsiveMixin from '../../../mixins/responsive';
 import DiscussionBaseController from '../base';
+import {track, trackActions} from '../../../utils/discussion-tracker';
 
 export default DiscussionBaseController.extend(
 	DiscussionModerationControllerMixin,
@@ -24,9 +25,27 @@ export default DiscussionBaseController.extend(
 
 		currentRouteName: Ember.computed.alias('applicationController.currentRouteName'),
 
+		wikiName: Ember.get(Mercury, 'wiki.siteName'),
+
+		displayedWikiName: Ember.computed(function () {
+			if (Ember.get(Mercury, 'wiki.id') === 734209) {
+				return 'Star Trek';
+			} else {
+				return this.get('wikiName');
+			}
+		}),
+
 		actions: {
 			setDays(days) {
 				this.get('target').send('setDays', days);
+			},
+
+			clickCommunityName() {
+				track(trackActions.CommunityNameClicked);
+			},
+
+			clickCommunityBadge() {
+				track(trackActions.CommunityBadgeClicked);
 			}
 		}
 	}
