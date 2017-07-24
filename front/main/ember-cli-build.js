9da78e0d530710d8bcdb26d67641924d3daed46d
Igor Rogatty <igor@wikia-inc.com>
2017-07-24 16:03:00 +0200

    XW-3739 | use template strings

--- front/main/ember-cli-build.js
+++ front/main/ember-cli-build.js
@@ -86,41 +86,41 @@ module.exports = function (defaults) {
 	});
 
 	// Files below are concatenated to assets/vendor.js
-	app.import(app.bowerDirectory + '/fastclick/lib/fastclick.js');
-	app.import(app.bowerDirectory + '/hammerjs/hammer.js');
-	app.import(app.bowerDirectory + '/headroom.js/dist/headroom.js');
-	app.import(app.bowerDirectory + '/jquery.cookie/jquery.cookie.js');
-	app.import(app.bowerDirectory + '/ember-hammer/ember-hammer.js');
-	app.import(app.bowerDirectory + '/i18next/i18next.js');
-	app.import(app.bowerDirectory + '/vignette/dist/vignette.js');
-	app.import(app.bowerDirectory + '/numeral/numeral.js');
-	app.import(app.bowerDirectory + '/weppy/dist/weppy.js');
-	app.import(app.bowerDirectory + '/visit-source/dist/visit-source.js');
-	app.import(app.bowerDirectory + '/Autolinker.js/dist/Autolinker.min.js');
-	app.import(app.bowerDirectory + '/tinycolor/dist/tinycolor-min.js');
+	app.import(`${app.bowerDirectory}/fastclick/lib/fastclick.js`);
+	app.import(`${app.bowerDirectory}/hammerjs/hammer.js`);
+	app.import(`${app.bowerDirectory}/headroom.js/dist/headroom.js`);
+	app.import(`${app.bowerDirectory}/jquery.cookie/jquery.cookie.js`);
+	app.import(`${app.bowerDirectory}/ember-hammer/ember-hammer.js`);
+	app.import(`${app.bowerDirectory}/i18next/i18next.js`);
+	app.import(`${app.bowerDirectory}/vignette/dist/vignette.js`);
+	app.import(`${app.bowerDirectory}/numeral/numeral.js`);
+	app.import(`${app.bowerDirectory}/weppy/dist/weppy.js`);
+	app.import(`${app.bowerDirectory}/visit-source/dist/visit-source.js`);
+	app.import(`${app.bowerDirectory}/Autolinker.js/dist/Autolinker.min.js`);
+	app.import(`${app.bowerDirectory}/tinycolor/dist/tinycolor-min.js`);
 	app.import('vendor/common.js');
 
 	if (app.env === 'test') {
 		// Fix for PhantomJS errors
-		app.import(app.bowerDirectory + '/es5-shim/es5-shim.min.js');
+		app.import(`${app.bowerDirectory}/es5-shim/es5-shim.min.js`);
 	}
 
 	// Assets which are lazy loaded
-	const cropperAssets = new Funnel(app.bowerDirectory + '/cropper/dist', {
+	const cropperAssets = new Funnel(`${app.bowerDirectory}/cropper/dist`, {
 			include: ['*.min.*'],
 			destDir: 'assets/vendor/cropper'
 		}),
-		jQueryAssets = new Funnel(app.bowerDirectory + '/jquery/dist', {
+		jQueryAssets = new Funnel(`${app.bowerDirectory}/jquery/dist`, {
 			include: ['*.min.*'],
 			destDir: 'assets/vendor/jquery'
 		}),
-		pontoAssets = new Funnel(app.bowerDirectory + '/ponto/web/src', {
+		pontoAssets = new Funnel(`${app.bowerDirectory}/ponto/web/src`, {
 			destDir: 'assets/vendor/ponto'
 		}),
-		numeralAssets = new Funnel(app.bowerDirectory + '/numeral/languages', {
+		numeralAssets = new Funnel(`${app.bowerDirectory}/numeral/languages`, {
 			destDir: 'assets/vendor/numeral'
 		}),
-		designSystemAssets = new Funnel(app.bowerDirectory + '/design-system/dist/svg/sprite.svg', {
+		designSystemAssets = new Funnel(`${app.bowerDirectory}/design-system/dist/svg/sprite.svg`, {
 			destDir: 'assets/design-system.svg'
 		});
 
