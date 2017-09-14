3b099fb9d3708fdfbed731a5933b424be205a1b5
Jakub Olek <bukaj.kelo+github@gmail.com>
2017-09-14 14:23:08 +0200

    XW-3822 | Replace __ with {} on server side configuration

--- server/app/app.js
+++ server/app/app.js
@@ -218,7 +218,7 @@ plugins = [
 		register: i18next,
 		options: {
 			i18nextOptions: {
-				resGetPath: path.join(localesPath, '/__lng__/__ns__.json'),
+				resGetPath: path.join(localesPath, '/{lng}/{ns}.json'),
 				ns: {
 					namespaces: ['main', 'auth', 'design-system', 'discussion'],
 					defaultNs: 'main'
