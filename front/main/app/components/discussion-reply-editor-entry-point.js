1c268b60a4e13053dea929e2d3450e807e5014d5
Igor Rogatty <igor@wikia-inc.com>
2017-10-04 18:14:58 +0200

    IRIS-5013 | show current user avatar in mobile reply editor entry point

--- front/main/app/components/discussion-reply-editor-entry-point.js
+++ front/main/app/components/discussion-reply-editor-entry-point.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 import DiscussionStickyComponentMixin from '../mixins/discussion-sticky-component';
 
-const {Component} = Ember;
+const {Component, inject} = Ember;
 
 export default Component.extend(
 	DiscussionStickyComponentMixin,
 	{
 		classNames: ['discussion-reply-editor-entry-point'],
 		containerSelector: '.discussion-editor-entry-point-container',
+
+		currentUser: inject.service(),
 
 		click() {
 			this.sendAction('setEditorActive', 'contributeEditor', true);
